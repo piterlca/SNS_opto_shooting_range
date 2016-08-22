5bae230ecc8f59a36b57e95f99d39dd74a051f5c
fortunaa <fortunaa.m@gmail.com>
2016-08-22 21:35:37 +0200

    1st hw proj

---
+++ src/opencv_mv_th.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : opencv_mv_th.c
+ Author      : SNS
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
